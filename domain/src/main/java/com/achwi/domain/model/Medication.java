2f763ec869fab09b7460e655a70ac4af89b57185
achwi00 <achwierut00@gmail.com>
2025-08-31 18:05:11 +0200

    chore: add javadoc comments

--- domain/src/main/java/com/achwi/domain/model/Medication.java
+++ domain/src/main/java/com/achwi/domain/model/Medication.java
@@ -2,6 +2,10 @@ package com.achwi.domain.model;
 
 import jakarta.persistence.*;
 
+/**
+ * Represents a medication that can be prescribed. Maps to the MEDICATION table in the database.
+ * Each medication has a unit (e.g. ML, MG) describing the measurement type.
+ */
 @Entity
 @Table(name = Medication.tableName)
 public class Medication {
--- domain/src/main/java/com/achwi/domain/model/Unit.java
+++ domain/src/main/java/com/achwi/domain/model/Unit.java
@@ -1,5 +1,6 @@
 package com.achwi.domain.model;
 
+/** Units of measurement supported in the system. Stored as strings in the database. */
 public enum Unit {
   ML,
   MG,
